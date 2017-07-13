246b8e643844fe072a897af22e268f74513b3cc3
Louis Tao <louistao630@sina.com>
2017-07-13 16:49:15 +0800

    添加动态代理的范例代码。

---
+++ src/main/java/com/louis/tool/pattern/proxyPattern/DynamicJDKBusinessProxyHandler.java
@@ -0,0 +1,30 @@
+package com.louis.tool.pattern.proxyPattern;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * 使用JDK创建动态代理，需要实现特定的接口
+ * 
+ * @author Louis Tao
+ *
+ */
+public class DynamicJDKBusinessProxyHandler implements InvocationHandler {
+
+	private BusinessAction ba;
+
+	public DynamicJDKBusinessProxyHandler(BusinessAction ba) {
+		this.ba = ba;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		writeLog(method);
+		return method.invoke(ba, args);
+	}
+
+	private void writeLog(Method method) {
+		System.out.println(method.getName() + " will invoked.");
+	}
+
+}
