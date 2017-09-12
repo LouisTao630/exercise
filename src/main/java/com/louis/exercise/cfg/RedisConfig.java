64d76e600d991e9012cb8ad3e6335e3534797cb0
Louis Tao <louistao630@sina.com>
2017-09-12 18:37:51 +0800

    apply redis

--- src/main/java/com/louis/exercise/cfg/RedisConfig.java
+++ src/main/java/com/louis/exercise/cfg/RedisConfig.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 @PropertySource("classpath:/com/louis/exercise/db/redis.properties")
@@ -14,9 +19,46 @@ public class RedisConfig {
 	@Value("${redis.host}")
 	private String host;
 
+	@Value("${redis.password}")
+	private String password;
+
+	@Value("${redis.db.index}")
+	private int index;
+
 	@Bean
 	public JedisPool jedisPool() {
 		JedisPool pool = new JedisPool(host);
 		return pool;
 	}
+
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig poolConfig) {
+
+		JedisConnectionFactory factory = new JedisConnectionFactory(poolConfig);
+		factory.setHostName(host);
+		factory.setPassword(password);
+		factory.setDatabase(index);
+		return factory;
+	}
+
+	@Bean
+	public JedisPoolConfig poolConfig() {
+		JedisPoolConfig poolConfig = new JedisPoolConfig();
+		return poolConfig;
+	}
+
+	@Bean
+	@Scope("prototype")
+	public Jedis jedis(JedisConnectionFactory factory) {
+		Jedis jedis = (Jedis) factory.getConnection().getNativeConnection();
+		return jedis;
+	}
+
+	@Bean
+	public StringRedisTemplate stringRedisTemplate(JedisConnectionFactory factory) {
+		StringRedisTemplate template = new StringRedisTemplate();
+		template.setConnectionFactory(factory);
+		return template;
+	}
+
 }
--- src/main/java/com/louis/exercise/db/mybatis/UserMapper.java
+++ src/main/java/com/louis/exercise/db/mybatis/UserMapper.java
@@ -10,4 +10,5 @@ public interface UserMapper {
 
 	@Select("SELECT First_Name AS firstName,Middle_Name as middleName,Last_Name as lastName,Birth_Date as birthday,Gender as gender,Address as address,City as city FROM user")
 	List<User> getUser();
+		
 }
